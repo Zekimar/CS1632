5551bdd12eb9e26233c0ad48458c1b9e5a2210ff
Kevin Good <zzffcrz@gmail.com>
2018-02-19 23:06:52 -0500

    fixed bug

--- d2/place_initialize_test.rb
+++ d2/place_initialize_test.rb
@@ -9,13 +9,13 @@ require_relative 'city_sin_9006'
 
 class PlaceInitTest < Minitest::Test
   #ensures that a Place object is not nil when created
-  def test_new_place_not_ni:
+  def test_new_place_not_nil
     pl = Place::new "t"
     refute_nil pl
   end
 
   #ensures argument is thrown
-  def test_wrong_arguments:
+  def test_wrong_arguments
     assert_raises "wrong number of arguments" do
       pl = Place::new "hello", "There"
     end
@@ -23,8 +23,9 @@ class PlaceInitTest < Minitest::Test
 
   #ensures that an exception is thrown if the name is not a String
   #EDGE CASE
-  def test_wrong_argument_type:
+  def test_wrong_argument_type
     assert_raises "name should be a string" do
       pl = Place::new 12
     end
   end
+end 
